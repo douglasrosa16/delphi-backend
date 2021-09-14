cd23a52886771eac7d8dc934504c91d9a5dbed5a
douglasrosa16 <douglasoliveira163425@gmail.com>
2021-09-14 11:26:59 -0400

    finalizado rotas de users

--- src/Controller/ServerReact.Controller.Users.pas
+++ src/Controller/ServerReact.Controller.Users.pas
@@ -10,14 +10,20 @@ uses
 
 procedure Registry(App : THorse);
 procedure Get(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+procedure GetID(Req: THorseRequest; Res: THorseResponse; Next: TProc);
 procedure Insert(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+procedure Update(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+procedure Delete(Req: THorseRequest; Res: THorseResponse; Next: TProc);
 
 implementation
 
 procedure Registry(App : THorse);
 begin
   App.Get('/users', Get);
+  App.Get('/users/:id', GetID);
   App.Post('/users', Insert);
+  App.Put('/users/:id', Update);
+  App.Delete('/users/:id', Delete);
 end;
 
 procedure Get(Req: THorseRequest; Res: THorseResponse; Next: TProc);
@@ -28,12 +34,36 @@ begin
     Res.Send<TJsonArray>(FDAO.Find);
 end;
 
+procedure GetID(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+  FDAO : iDAOGeneric<TUSER>;
+begin
+    FDAO := TDAOGeneric<TUSER>.New;
+    Res.Send<TJsonObject>(FDAO.Find(Req.Params.Items['id']));
+end;
+
 procedure Insert(Req: THorseRequest; Res: THorseResponse; Next: TProc);
 var
   FDAO : iDAOGeneric<TUSER>;
 begin
   FDAO := TDAOGeneric<TUSER>.New;
   Res.Send<TJSONObject>(FDAO.Insert(Req.Body<TJSONObject>));
+end;
+
+procedure Update(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+  FDAO : iDAOGeneric<TUSER>;
+begin
+  FDAO := TDAOGeneric<TUSER>.New;
+  Res.Send<TJSONObject>(FDAO.Update(Req.Body<TJSONObject>));
+end;
+
+procedure Delete(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+  FDAO : iDAOGeneric<TUSER>;
+begin
+  FDAO := TDAOGeneric<TUSER>.New;
+  Res.Send<TJSONObject>(FDAO.Delete('ID', Req.Params.Items['id']));
 end;
 
 end.
